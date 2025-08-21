45d529acb75ffc6c5a1106f3c07983c06c8802f1
MHiirwa <m.hirwa1@alustudent.com>
2025-08-21 21:31:30 +0200

    insert data into students tables

--- student-performance-db.sql
+++ student-performance-db.sql
@@ -29,3 +29,21 @@ CREATE TABLE python_grades (
     grade_obtained DECIMAL(5,2) CHECK (grade_obtained BETWEEN 0 AND 100),
     FOREIGN KEY (student_id) REFERENCES students(student_id)
 );
+
+-- Insert  data into students table (15 students)
+INSERT INTO students (student_name, intake_year) VALUES
+('Alice Uwase', 2022),
+('Bob Mugisha', 2022),
+('Claire Imanishimwe', 2023),
+('David Niyonzima', 2022),
+('Emma Mukamana', 2023),
+('Frank Habimana', 2022),
+('Grace Uwimbabazi', 2023),
+('Henry Twahirwa', 2022),
+('Irene Mutoni', 2023),
+('James Nkusi', 2022),
+('Karen Uwase', 2023),
+('Leo Ndayisaba', 2022),
+('Mia Uwimana', 2023),
+('Nathan Iradukunda', 2022),
+('Olivia Nyirahabimana', 2023);
